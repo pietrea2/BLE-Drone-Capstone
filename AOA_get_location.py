886a153e8fd98d3b179bedb0f4c082643c9e69d1
Adam Pietrewicz <adam.pietrewicz@mail.utoronto.ca>
2023-02-11 17:50:01 -0500

    Printing tag angle location

--- AOA_get_location.py
+++ AOA_get_location.py
@@ -1,4 +1,57 @@
-
+import serial
+import time
 
 def AOA_get_location():
-    print("getting the location information from the AOA anchor...")
+    print("Reading anchor data")
+
+    serial_con = serial.Serial('COM12', 115200, parity=serial.PARITY_NONE, rtscts=1, timeout=1, stopbits=1);
+    print(serial_con);
+
+    serial_con.write("AT\r".encode()) #attention
+    for i in range(3):
+        line = serial_con.readline()
+        print(line)
+
+
+    serial_con.write("AT+GMM\r".encode()) #model ID
+    for i in range(3):
+        line = serial_con.readline()
+        print(line)
+
+    serial_con.write("AT+UMLA=1\r".encode()) #local address
+    for i in range(3):
+        line = serial_con.readline()
+        print(line)
+
+    serial_con.write("ATI9\r".encode()) #software ID
+    for i in range(3):
+        line = serial_con.readline()
+        print(line)
+
+    serial_con.write("AT+UDFENABLE=1\r".encode()) #Direction finding enable (track / angle calc)
+    for i in range(3):
+        line = serial_con.readline()
+        print(line)
+
+    serial_con.write("+UDFFILT\r".encode()) #read response from Direction finding filter
+    for i in range(50):
+        line = serial_con.readline()
+        print(line)
+
+    a = 1
+    while a == 1:
+        line = serial_con.readline()
+        print(line)
+        time.sleep(0)
+
+    
+    
+
+    
+
+
+
+
+
+
+AOA_get_location()
