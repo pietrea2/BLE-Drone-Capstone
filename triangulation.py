4cf259859f14bfe39f66182a67513882e072f167
Adam Pietrewicz <adam.pietrewicz@mail.utoronto.ca>
2023-02-12 20:14:44 -0500

    Starting triangulation, 2 calc methods to try

--- triangulation.py
+++ triangulation.py
@@ -1,4 +1,36 @@
+import math
 
 
-def triangulation():
+
+def triangulation(num_of_anchors, azimuth1, azimuth2, elevation):
     print("triangulating the location of the drones to cartesian...")
+
+
+    if(num_of_anchors == 2):
+
+
+        #----------Law of Sines Method-------------------------------------------------
+        #Will calculate the location of drone relative to anchor #1
+        #Have:
+        #   azimuth1 = theta
+        #   90 - elevation = phi
+
+        #Need to calculate rho:
+        rho = 0
+        
+        theta = azimuth1
+        phi = 90 - elevation
+
+
+        x = rho * math.sin( math.radians(phi) ) * math.cos( math.radians(theta) )
+        y = rho * math.sin( math.radians(phi) ) * math.sin( math.radians(theta) )
+        z = rho * math.cos( math.radians(phi) )
+
+
+
+        #-----------Method #2----------------------------------------------------------
+        #Source:
+        #https://www.omnicalculator.com/math/triangulation
+
+
+
